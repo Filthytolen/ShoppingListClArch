5fe7860ec12b2085e58dad56e828fe1ea931dbc5
FilthyTolen <abortsov012@gmail.com>
2022-05-26 12:54:54 +0300

    Added ViewModel to pres layer. Added observer for a shopList and method for getting it in MainActivity.kt.

---
+++ app/src/main/java/com/example/shoppinglistclarch/presentation/MainViewModel.kt
@@ -0,0 +1,38 @@
+package com.example.shoppinglistclarch.presentation
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.example.shoppinglistclarch.data.ShopListRepositoryImpl
+import com.example.shoppinglistclarch.domain.DeleteShopItemUseCase
+import com.example.shoppinglistclarch.domain.EditShopItemUseCase
+import com.example.shoppinglistclarch.domain.GetShopListUseCase
+import com.example.shoppinglistclarch.domain.ShopItem
+
+class MainViewModel: ViewModel() {
+
+    private val repository = ShopListRepositoryImpl //I know it's wrong but idk dagger
+
+    private val getShopListUseCase = GetShopListUseCase(repository)
+    private val deleteShopItemUseCase = DeleteShopItemUseCase(repository)
+    private val editShopItemUseCase = EditShopItemUseCase(repository)
+
+    val shopList = MutableLiveData<List<ShopItem>>()
+
+
+    fun getShopList() {
+        val list = getShopListUseCase.getShopList()
+        shopList.value = list
+    }
+
+    fun deleteShopItem(shopItem: ShopItem) {
+        deleteShopItemUseCase.deleteShopItem(shopItem)
+        getShopList()
+    }
+
+    fun changeEnableState(shopItem: ShopItem) {
+        val newItem = shopItem.copy(enabled = !shopItem.enabled)
+        editShopItemUseCase.editShopItem(newItem)
+        getShopList()
+    }
+}
