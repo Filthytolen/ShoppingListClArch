b9f38e985ca68346707c8fa314492489819c2c9d
FilthyTolen <abortsov012@gmail.com>
2022-05-29 08:36:21 +0300

    Done work with ViewModel

---
+++ app/src/main/java/com/example/shoppinglistclarch/presentation/ShopItemActivity.kt
@@ -0,0 +1,12 @@
+package com.example.shoppinglistclarch.presentation
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.shoppinglistclarch.R
+
+class ShopItemActivity: AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_shop_item)
+    }
+}
